a4db6c92403b211214c02b4dfcaf2d674eb83f74
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2023-10-19 09:35:07 -0400

    phase1 results inserted sucessfully

--- insert_job_results.py
+++ insert_job_results.py
@@ -1,13 +1,33 @@
+import os
 import fire
 from pathlib import Path
 from models.sql_connection import SQL_Connection
 from models.job_processor import Processor
+from tqdm import tqdm
 
-def insert_job_results(job_path, db_config_file="sql_configs.yaml", db_config_name="Local"):
-    job_path = Path(job_path)
+def insert_job_results(jobs_dirs_base=None,job_path=None, db_config_file="sql_configs.yaml", db_config_name="Local"):
+    """
+    Insert MutPred2 results into database
+
+    Requires either jobs_dirs_base or job_path
+
+    Optional Arguments
+    - jobs_dirs_base : path to directory containing folders for each job to be processed
+    - job_path : path to job to be processed
+    """
+    if (jobs_dirs_base is not None and job_path is not None) or (jobs_dirs_base is None and job_path is None):
+        raise ValueError("specify either jobs_dirs_base or job_path")
+    elif jobs_dirs_base is not None:
+        print(f"processing jobs in directory {jobs_dirs_base}")
+        jobs_dirs_base = Path(jobs_dirs_base)
+        jobs = [jobs_dirs_base/j for j in os.listdir(jobs_dirs_base)]
+    else:
+        print(f"processing job {job_path}")
+        jobs = [Path(job_path)]
     con = SQL_Connection(db_config_name, db_config_file)
     processor = Processor(con)
-    processor.process_job(job_path)
+    for job in tqdm(jobs):
+        processor.process_job(job)
 
 if __name__ == "__main__":
-    insert_job_results()
+    fire.Fire(insert_job_results)
