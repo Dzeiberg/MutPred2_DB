8eacd3c06c67cc8d45c5ec33e5518a188f700be4
Daniel Zeiberg <zeiberg.d@northeastern.edu>
2024-05-09 16:21:02 -0400

    do not crash on failed job, log and continue

--- mutpred2_db/process_job.py
+++ mutpred2_db/process_job.py
@@ -1,10 +1,8 @@
 from models.job_processor import Processor
 from models.sql_connection import SQL_Connection, initialize_mechanisms
 from fire import Fire
-# from joblib import Parallel, delayed
-
+from sqlalchemy.exc import DatabaseError
 from tqdm import tqdm
-from functools import partialmethod
 # tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
 
 class FileCursor:
@@ -12,7 +10,6 @@ class FileCursor:
         self.file = open(filepath, 'w')
 
     def __exit__(self):
-        # self.file.close()
         super().__exit__()
 
     def execute(self, query, data=None):
@@ -48,23 +45,25 @@ def process_job_list(sql_config_name : str, sql_config_file : str,
         raise ValueError("Either job_list_file or job_path must be provided")
     sql_connection = SQL_Connection(sql_config_name, sql_config_file)
     cursor, cnx = sql_connection.open()
-    # cnx = FileConnection("initialize.sql")
-    # cursor = cnx._cursor
     initialize_mechanisms(cursor,cnx)
-    # cursor.file.close()
-    def process_job(job_dir : str, sql_config_name : str, sql_config_file : str,**run_option_kwargs):
-        # print(f"Processing {job_dir}")
-        cursor, cnx = sql_connection.open()
-        # cnx = FileConnection(f"{job_dir}/insert.sql")
-        # cursor = cnx._cursor
+    def process_job(cursor, cnx, job_dir : str, sql_config_name : str, sql_config_file : str,**run_option_kwargs):
         job_processor = Processor(cursor, cnx)
         job_processor.process(job_dir, write_to_db=True,**run_option_kwargs)
-        sql_connection.close(cursor,cnx)
-        # cursor.file.close()
-    [process_job(job,
-                sql_config_name,
-                sql_config_file,
-                **run_option_kwargs) for job in tqdm(job_list)]
-
+    failed_jobs = []
+    for job in tqdm(job_list):
+        try:
+            process_job(cursor, cnx, job, sql_config_name, sql_config_file, **run_option_kwargs)
+        except DatabaseError as e:
+            print(f"JOB_ERROR: Error processing {job}")
+            failed_jobs.append(job)
+            print(e)
+            continue
+    sql_connection.close(cursor,cnx)
+    if len(failed_jobs) > 0:
+        print("Failed jobs:")
+        for job in failed_jobs:
+            print(job)
+    else:
+        print("All jobs processed successfully")
 if __name__ == "__main__":
     Fire(process_job_list)
